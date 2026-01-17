8bfd9b9d505861759b1b425b316a0de3229aadf1
tuneyourdrums <johnrogerstafford@gmail.com>
2026-01-17 11:30:38 -0800

    update scan with methods

--- scanPeripherals.lua
+++ scanPeripherals.lua
@@ -2,6 +2,18 @@ local function scanPeripherals()
     local list = peripheral.getNames()
     for _,p in ipairs(list) do
         print(p)
+        local methods = peripheral.getMethods(p)
+        local counter = 0
+        for _,m in ipairs(methods) do
+            print(m)
+            counter = counter + 1
+            if counter >= 15 then
+                sleep(15)
+                counter = 0
+                term.clear()
+                print(p)
+            end
+        end
     end
 end
 
