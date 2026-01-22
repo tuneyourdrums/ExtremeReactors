a6783d1d2a57f8b174e3c5a1f506aa1ec892a5dc
tuneyourdrums <johnrogerstafford@gmail.com>
2026-01-21 20:15:43 -0800

    fix installer

--- installer.lua
+++ installer.lua
@@ -9,6 +9,7 @@ local base =
   "https://raw.githubusercontent.com/tuneyourdrums/ExtremeReactors/refs/heads/main/"
 
 for _, file in ipairs(files) do
+  shell.run("rm", file)
   print("Downloading " .. file)
   shell.run("wget", base .. file, file)
 end
