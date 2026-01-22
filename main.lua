861704209495df1560d345296339c48d5096328b
tuneyourdrums <johnrogerstafford@gmail.com>
2026-01-21 21:39:39 -0800

    fix percentage

--- main.lua
+++ main.lua
@@ -72,7 +72,7 @@ while true do
             t:setFluidFlowRateMax(1740)
             t:setInductorEngaged(true)
             t:setVentOverflow()
-            writeNewLine("Turbine "..num.." Charge %: ",math.floor(t:getEnergyStored()/t:getEnergyCapacity()))
+            writeNewLine("Turbine "..num.." Charge %: ",math.floor(t:getEnergyStored()/t:getEnergyCapacity()*100))
             writeNewLine("Turbine "..num.." kFE/t: ",math.floor(t:getEnergyProducedLastTick()/100)/10)
         end
     sleep(1)
