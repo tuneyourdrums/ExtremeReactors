914fd4bf23f2cf972ffa21a52f1fd283e2f004d5
tuneyourdrums <johnrogerstafford@gmail.com>
2026-01-21 21:11:39 -0800

    add writeNewLine

--- main.lua
+++ main.lua
@@ -52,20 +52,27 @@ for _, r in ipairs(devices.reactors) do
     r:setRods(50)
 end
 
+local function writeNewLine(string)
+    write(string)
+    local _,y = term.getCursorPos()
+    term.setCursorPos(1,y+1)
+end
+
 while true do
         term.clear()
         term.setCursorPos(1, 1)
 
         for num, r in ipairs(devices.reactors) do
-            print("Fuel usage:", r:getFuelUsage())
+            writeNewLine("Fuel usage:", r:getFuelUsage())
+            
         end
         
         for num, t in ipairs(devices.turbines) do
             t:setFluidFlowRateMax(1740)
             t:setInductorEngaged(true)
             t:setVentOverflow()
-            print("Turbine", num, "Capacity: ", t:getEnergyCapacity())
-            print("RF/t", num, ": ", t:getEnergyProducedLastTick())
+            writeNewLine("Turbine"..num.."Capacity: "..t:getEnergyCapacity())
+            writeNewLine("RF/t"..num..": "..t:getEnergyProducedLastTick())
         end
     sleep(1)
 end
