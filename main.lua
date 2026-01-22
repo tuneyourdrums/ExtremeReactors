047edac54c2a511b910decb2d7b9efa672520cf6
tuneyourdrums <johnrogerstafford@gmail.com>
2026-01-21 20:27:38 -0800

    update main for turbines

--- main.lua
+++ main.lua
@@ -1,25 +1,43 @@
 local ExtremeReactor = require("ExtremeReactor")
 
+local ExtremeReactor = require("ExtremeReactor")
+
 local function scanForReactors()
     term.clear()
     term.setCursorPos(1, 1)
     print("Initializing...")
 
+    local result = {
+        reactors = {},
+        turbines = {}
+    }
+
     for _, name in ipairs(peripheral.getNames()) do
         local pType = peripheral.getType(name)
-        local reactors = {}
-        local turbines = {}
-        if pType and string.find(pType, "Reactor") then
+
+        if pType and string.find(pType, "BigReactor-Reactor") then
             if peripheral.call(name, "mbIsAssembled") == true then
-                print("ExtremeReactor found!")
-                return ExtremeReactor.new(name)
+                print("ExtremeReactor found:", name)
+                table.insert(result.reactors, ExtremeReactor.new(name))
+            end
+        elseif pType and string.find(pType, "BigReactor-Turbine") then
+            if peripheral.call(name, "mbIsAssembled") == true then
+                print("ExtremeTurbine found:", name)
+                table.insert(result.reactors, ExtremeReactor.new(name))
             end
         end
     end
 
-    print("Reactor not found.")
-    return nil
+    if #result.reactors == 0 then
+        print("No reactors found.")
+    end
+    if #result.turbines == 0 then
+        print("No turbines found.")
+    end
+
+    return result
 end
+
 
 local reactor = scanForReactors()
 
